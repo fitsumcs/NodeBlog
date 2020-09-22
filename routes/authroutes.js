6f5d3ffeeb436b772d5761599cc1890a1bc232c7
3020 <fitsumcs@gmail.com>
2020-09-22 14:42:46 +0300

    Adding css on pagination and fixing some bugs on route

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -19,7 +19,7 @@ router.post('/register', (req, res) => {
             return res.redirect('/register');
         }
         passport.authenticate('local')(req, res, () => {
-            res.redirect('/blogs');
+            res.redirect('/');
         });
     });
 });
@@ -29,7 +29,7 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 router.post('/login', passport.authenticate('local', {
-    successRedirect: '/blogs',
+    successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
 }), (req, res) => {});
